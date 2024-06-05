e2f1bd74ad3e08e49872b00294aa576ce31da4e2
Sanudin <mailto.sanudin@gmail.com>
2024-06-05 08:49:55 +0700

    fix functions

--- server.js
+++ server.js
@@ -1,72 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const { Client } = require("@notionhq/client");
-const serverless = require("serverless-http");
 
 const app = express();
-const router = express.Router();
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 app.use(express.static("public"));
 app.use(express.json());
 
-// Index
-router.get("/  ", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-// Send Wedding Messages
-router.post("/wedding-msg", async function (request, response) {
-  const dbID = process.env.NOTION_PAGE_ID_WEDDING_MSG;
-  const { name, msg } = request.body;
-
-  try {
-    const wedMsg = await notion.pages.create({
-      parent: {
-        type: "database_id",
-        database_id: dbID,
-      },
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: name,
-              },
-            },
-          ],
-        },
-        Message: {
-          rich_text: [
-            {
-              text: {
-                content: msg,
-              },
-            },
-          ],
-        },
-      },
-      children: [
-        {
-          object: "block",
-          heading_2: {
-            rich_text: [
-              {
-                text: {
-                  content: msg,
-                },
-              },
-            ],
-          },
-        },
-      ],
-    });
-    response.json({ message: "success!", data: wedMsg.properties });
-  } catch (error) {
-    response.json({ message: "error", error });
-  }
-});
-
-app.use("/.netlify/functions/api/", router);
 // Index
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
@@ -126,5 +67,3 @@ app.post("/wedding-msg", async function (request, response) {
 const listener = app.listen(process.env.PORT || 1001, () => {
   console.log(`Server started on port ${listener.address().port}`);
 });
-
-module.exports.handler = serverless(app);
