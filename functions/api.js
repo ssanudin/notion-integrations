345172d6b9c1dcc851d3c44cd4efb6729dad2fb2
Sanudin <mailto.sanudin@gmail.com>
2024-06-05 09:08:10 +0700

    update

--- functions/api.js
+++ functions/api.js
@@ -4,8 +4,11 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
-router.get("/  ", (request, response) => {
+router.get("/", (request, response) => {
   response.send("App is running..");
+});
+router.get("/api", (request, response) => {
+  response.send("api path");
 });
 
 app.use("/.netlify/functions/api/", router);
